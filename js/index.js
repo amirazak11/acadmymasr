c7d374be86ee88762688a038216f570114b8621a
unknown <you@example.com>
2023-02-18 18:03:53 +0200

    add new sections

--- js/index.js
+++ js/index.js
@@ -1,6 +1,16 @@
 let navButton = document.getElementById("navbutton");
 let cardsContainer = document.getElementById("cards-container");
-let cards = [
+let priceContainer = document.getElementById("price-container");
+let categorycards = [
+  { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
+  { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
+  { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
+  { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
+  { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
+  { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
+
+]
+let pricecards = [
   { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
   { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
   { img: "", label: " هذا النص هو مثال", text: "مده الدراسة 4 سنوات "},
@@ -10,8 +20,9 @@ let cards = [
 
 ]
 
+
 let cartona = ``;
-for (let i = 0; i < cards.length; i++) {
+for (let i = 0; i < categorycards.length; i++) {
   cartona += `   
     <div class="col-lg-4  col-sm-12 ">
     <div class="card ">
@@ -21,8 +32,8 @@ for (let i = 0; i < cards.length; i++) {
 <div class=" text-about-section">
 
 
-<h6>${cards[i].label}</h6>
-<p>${cards[i].text}</p>
+<h6>${categorycards[i].label}</h6>
+<p>${categorycards[i].text}</p>
 </div>
 
 </div>
@@ -31,7 +42,40 @@ for (let i = 0; i < cards.length; i++) {
 }
 
 cardsContainer.innerHTML = cartona;
+let pricecartona = ``;
+for (let i = 0; i < categorycards.length; i++) {
+  pricecartona += `   
+  <div class="col-lg-4  col-sm-12 ">
+  <div class="card-price ">
+<h3>التخصص</h3>
+<p>هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة، لقد تم توليد هذا النص من مولد النص</p>
+<h2>15000 <span>/جنيها</span></h2>
+<div class="list">
+<h4>تشمل المصاريف</h4>
+<ul>
+<li>
+<i class="fa-solid fa-check"></i>
+هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة،</li>
+<li>
+<i class="fa-solid fa-check"></i>
+هذا النص هو مثال لنص يمكن أن </li>
+<li>
+<i class="fa-solid fa-check"></i>
+هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة،</li>
+<li>
+<i class="fa-solid fa-check"></i>
+هذا النص هو مثال لنص يمكن أن </li>
+<li>
+<i class="fa-solid fa-check"></i>
+هذا النص هو مثال لنص يمكن أن يستبدل في نفس المساحة،</li>
+</ul>
+</div>
+</div>
+</div>
+    `
+}
 
+priceContainer.innerHTML = pricecartona;
 $(".navbar-toggler").click(function () {
   $(".nav-item-res").animate({
     opacity: "1",
